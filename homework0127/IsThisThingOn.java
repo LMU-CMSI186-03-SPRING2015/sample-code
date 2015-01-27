792f8800e2dc456c23cb850f0f6c0a75bbf1dd57
Anthony Khayat <akhayat@users.noreply.github.com>
2015-01-27 15:29:25 -0800

    Committed IsThisThingOn.java
    Committed the skeleton for IsThisThingOn, the first homework assignment.

---
+++ homework0127/IsThisThingOn.java
@@ -0,0 +1,13 @@
+public class IsThisThingOn {
+    public static void main(String[] args) {
+        String testString = "Testing, testing, is this thing on?";
+        System.out.println(countTheVowels(testString));
+    }
+
+    public static int countTheVowels(String text) {
+      int count = 0;
+      //TODO: Finish this.
+      return count;
+    }
+
+}
